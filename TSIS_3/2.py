5ead0fddf1a54aa64b5818194136a9f8b582e34e
pp2-22B030182 <123373226+pp2-22B030182@users.noreply.github.com>
2023-02-07 15:31:21 +0600

    Delete TSIS_3 directory

--- TSIS_3/2.py
+++
@@ -1,6 +0,0 @@
-def faran(cels):
-    return (cels-32) * (5 / 9) 
-    
-cels = float(input())
-
-print(faran(cels))
--- TSIS_3/3.py
+++
@@ -1,8 +0,0 @@
-def solve(numheads,numlegs):
-    r = (legs - 2*h) / 2
-    c = h - r
-    return r, c
-
-h = 35
-legs = 94
-print(*solve(h,legs))
