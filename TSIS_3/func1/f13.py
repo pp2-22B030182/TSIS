235629f2635703fc9805c8869d04c56d3656f85a
pp2-22B030182 <makhmutovmaksat05@gmail.com>
2023-02-08 20:36:34 +0600

    tsis3

---
+++ TSIS_3/func1/f13.py
@@ -0,0 +1,5 @@
+import f4
+
+li = input().split()  
+
+print(f4.prime(li))
---
+++ TSIS_3/func1/f4.py
@@ -0,0 +1,22 @@
+import math
+   
+
+
+def isprime(num):
+    if(num == 1):
+        return False
+    for i in range (2, int(math.sqrt(num)) + 1):
+        if(num % i == 0):
+            return False
+    return True 
+
+
+def prime(li):
+    res = []
+    for num in li:
+        num = int(num)
+        if(isprime(num) == True):
+            res.append(num)
+    return res
+
+print("Welcome to f4.py")
